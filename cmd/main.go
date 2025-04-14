7062f985e69cc96b1ca98c0dedeb1d97767d9eeb
Bruce Becker <brucellino@protonmail.ch>
2025-04-14 10:14:08 +0200

    refactor: move nomad validation to nomad client package
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/brucellino/test-operator/pkg/nomad"
 	"github.com/charmbracelet/log"
@@ -41,24 +39,19 @@ func run(args []string) error {
 		return fmt.Errorf("failed to create Nomad client: %w", err)
 	}
 
-	if err := validateNomadConnection(client); err != nil {
+	if err := nomad.ValidateNomadConnection(client); err != nil {
 		log.Errorf("Failed to validate Nomad connection: %v", err)
 		return fmt.Errorf("failed to validate Nomad connection %w", err)
 	}
 
 	log.Info("Nomad connection valid.")
-
 	log.Info("Starting Consumer")
 	// operator was stepup
 	operator, err := nomad.NewOperator(client)
 	if err != nil {
 		return fmt.Errorf("Failed to create operator %w", err)
 	}
-	// consumer := NewAllocationConsumer(client, operator.onAllocation())
-	log.Infof("Operator is %v", *operator)
-
 	// Make test parts - critical, Inner, outer, full
-
 	// Make a new Job Consumer
 	eventConsumer := nomad.NewEventConsumer(client, operator.OnEvent)
 	// shutdown part
@@ -72,52 +65,11 @@ func run(args []string) error {
 		eventConsumer.StopEventConsumer()
 		os.Exit(0)
 	}()
+
+	// The initial critical tests are executed by StartCriticalTests()
+	// eventConsumer.StartCriticalTests()
+
+	// We then start an event consumer to watch for the job to finish
 	eventConsumer.StartEventConsumer()
 	return nil
 } // run
-
-// validateNomadConnection is a utility private function to check that the Nomad client can interact with the Nomad API.
-func validateNomadConnection(client *api.Client) error {
-	// try to list nodes - if there are no nodes, then the api connection is not valid.
-	nodes, _, err := client.Nodes().List(&api.QueryOptions{})
-
-	if err != nil {
-		log.Errorf("Failed to list nodes: %v", err)
-		return fmt.Errorf("Failed to list nodes: %w", err)
-	}
-
-	if len(nodes) == 0 {
-		log.Error("No nodes found - check if NOMAD_TOKEN has correct permissions or is set.")
-		return fmt.Errorf("no nodes found - check if NOMAD_TOKEN has correct permissions or is set")
-	}
-
-	// see if we can access all topics
-	topics := map[api.Topic][]string{
-		api.TopicNode: {"*"},
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	eventsCh, err := client.EventStream().Stream(ctx, topics, 0, &api.QueryOptions{})
-
-	if err != nil {
-		log.Errorf("Failed to validate nomad event stream: %v", err)
-		return fmt.Errorf("Failed to validate nomad nomad event stream: %w", err)
-	}
-
-	// wait for at least one event, even a heartbeat
-	select {
-	case event := <-eventsCh:
-		if event.Err != nil {
-			log.Errorf("Failed to validate nomad event stream: %v", err)
-			return fmt.Errorf("Failed to validate nomad nomad event stream: %w", err)
-		}
-
-	case <-ctx.Done():
-		log.Errorf("Timeout waiting for event stream connection")
-		return fmt.Errorf("Timeout waiting for event stream connection")
-	}
-
-	return nil
-}
--- pkg/nomad/client.go
+++ pkg/nomad/client.go
@@ -15,13 +15,60 @@ type Operator struct {
 	onEvent func(eventType string, job *api.Job)
 }
 
+// EventConsumer is a struct which contains a client, a function to handle events and a function to stop the consumer
 type EventConsumer struct {
 	client  *api.Client
 	onEvent func(eventType string, job *api.Job)
 	stop    func()
 }
 
-// NewOperator is a public function which returns a new Operator instance and optional error
+// validateNomadConnection is a utility private function to check that the Nomad client can interact with the Nomad API.
+func ValidateNomadConnection(client *api.Client) error {
+	// try to list nodes - if there are no nodes, then the api connection is not valid.
+	nodes, _, err := client.Nodes().List(&api.QueryOptions{})
+
+	if err != nil {
+		log.Errorf("Failed to list nodes: %v", err)
+		return fmt.Errorf("Failed to list nodes: %w", err)
+	}
+
+	if len(nodes) == 0 {
+		log.Error("No nodes found - check if NOMAD_TOKEN has correct permissions or is set.")
+		return fmt.Errorf("no nodes found - check if NOMAD_TOKEN has correct permissions or is set")
+	}
+
+	// see if we can access all topics
+	topics := map[api.Topic][]string{
+		api.TopicNode: {"*"},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	eventsCh, err := client.EventStream().Stream(ctx, topics, 0, &api.QueryOptions{})
+
+	if err != nil {
+		log.Errorf("Failed to validate nomad event stream: %v", err)
+		return fmt.Errorf("Failed to validate nomad nomad event stream: %w", err)
+	}
+
+	// wait for at least one event, even a heartbeat
+	select {
+	case event := <-eventsCh:
+		if event.Err != nil {
+			log.Errorf("Failed to validate nomad event stream: %v", err)
+			return fmt.Errorf("Failed to validate nomad nomad event stream: %w", err)
+		}
+
+	case <-ctx.Done():
+		log.Errorf("Timeout waiting for event stream connection")
+		return fmt.Errorf("Timeout waiting for event stream connection")
+	}
+
+	return nil
+}
+
+// NewOperator is a public function which returns a new Operator instance and optional error.
 func NewOperator(client *api.Client) (*Operator, error) {
 	return &Operator{
 		client: client,
@@ -44,11 +91,11 @@ func NewEventConsumer(client *api.Client, onEvent func(eventType string, job *ap
 	}
 }
 
+// StopEventConsumer is a public function which is used to stop the event consumer during shutdown.
 func (ec *EventConsumer) StopEventConsumer() {
 	if ec.stop != nil {
 		ec.stop()
 	}
-
 }
 
 // StartEventConsumer is a function which receives a EventConsumer type
@@ -60,6 +107,7 @@ func (ec *EventConsumer) StartEventConsumer() {
 	ec.consume(ctx)
 }
 
+// consumer is a private function which is used to consumer the event. It receives the event payload from Nomad and performs business logic on it.
 func (ec *EventConsumer) consume(ctx context.Context) error {
 	log.Info("Consuming events")
 	// this is the index of the event
@@ -69,10 +117,10 @@ func (ec *EventConsumer) consume(ctx context.Context) error {
 		api.TopicJob: {"*"},
 	}
 
-	// start the event stream
+	// eventsClient reads the event stream, which will later loop over
+	log.Info("Establishing event stream")
 	eventsClient := ec.client.EventStream()
 
-	log.Info("Establishing event stream")
 	// create the channel for the events
 	eventCh, err := eventsClient.Stream(ctx, topics, index, &api.QueryOptions{})
 	if err != nil {
@@ -80,9 +128,10 @@ func (ec *EventConsumer) consume(ctx context.Context) error {
 		return fmt.Errorf("Failed to create event stream: %w", err)
 	}
 
+	// We start a ticker from now, in order to check if events are still coming.
+	// We include heartbeat events in our check, but skip them in the event loop for processing later.
 	lastEventTime := time.Now()
 	heartbeatCheck := time.NewTicker(30 * time.Second)
-
 	defer heartbeatCheck.Stop()
 
 	// Loop over events in the stream
@@ -91,18 +140,34 @@ func (ec *EventConsumer) consume(ctx context.Context) error {
 		// If the context is closed, return no error.
 		case <-ctx.Done():
 			return nil
-		// If it's a heartbeat event, check when last we got
+		// If it's a heartbeat event, check when last we got an event.
+		// if it's more than 10 minutes ago, log a warning
+		// C is a ticker channel. This case deals with ticker events.
 		case <-heartbeatCheck.C:
 			if time.Since(lastEventTime) > 10*time.Minute {
 				log.Warn("No events received in the last 10 minutes")
-				return fmt.Errorf("No events received in the last 10 minutes")
+				// return fmt.Errorf("No events received in the last 10 minutes")
+				continue
 			}
+		// eventCh is an event stream channel. This case deals with event stream events.
 		case jobEvent := <-eventCh:
 			if jobEvent.Err != nil {
 				log.Errorf("Error receiving event: %v", jobEvent.Err)
 				continue
 			}
-			log.Infof("Received job event: %v", jobEvent)
+			// loop over the events received in the stream and handle them
+			for _, event := range jobEvent.Events {
+				log.Infof("Received event: %v", event.Topic)
+				if event.Topic == "Job" {
+					job, err := event.Job()
+					if err != nil {
+						log.Errorf("Error getting job: %v", err)
+						continue
+					}
+					// If the job event is a complete event, construct the cdevent and submit the new batch job
+					log.Infof("job name: %s", *job.Name)
+				}
+			}
 		}
 	}
 }
